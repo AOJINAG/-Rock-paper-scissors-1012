67710321a06c1660464ba2eb36a8feb761b4a528
Omar Shaikh <92153152+Omars214@users.noreply.github.com>
2021-11-27 18:34:10 +0400

    Just a test. Don't need to do anything.

---
+++ Project 2/RPS_Settings.js
@@ -0,0 +1,11 @@
+function background1() {
+    document.body.style.backgroundImage = "url('rock-paper-scissors.jpg')";
+}
+
+function background2() {
+    document.body.style.backgroundImage = "url('rock-paper-scissors-2.jpg')";
+}
+
+function background3() {
+    document.body.style.backgroundImage = "url('rock-paper-scissors-3.jpg')";
+}
